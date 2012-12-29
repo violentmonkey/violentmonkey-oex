1ea472ad022c163bea84c690f566c08901141805
Gerald <gera2ld@163.com>
2012-12-29 14:47:47 +0800

    ui modified

--- popup.js
+++ popup.js
@@ -2,54 +2,53 @@ function $(i){return document.getElementById(i);}
 var bg=opera.extension.bgProcess,P=$('popup'),_=bg.getI18nString,
 	tab=bg.opera.extension.tabs.getFocused();
 function loadItem(d,c){
-	if(c) d.classList.remove('disabled');
-	else d.classList.add('disabled');
+	if(c) {
+		d.firstChild.innerText=d.symbol;
+		d.classList.remove('disabled');
+	} else {
+		d.firstChild.innerText='';
+		d.classList.add('disabled');
+	}
 }
 function addItem(h,t,c){
 	var d=document.createElement('label'),s='';
-	if('data' in c) s+='<input type=checkbox>';
-	else s+='<span>>></span>';
-	d.innerHTML=s+h;
+	d.innerHTML='<span></span>'+h;
 	if(t) {if(typeof t!='string') t=h;d.title=t;}
 	d.className='ellipsis';
 	P.appendChild(d);
-	if('data' in c){
-		loadItem(d,c.data);
-		d.firstChild.checked=!!c.data;
-		delete c.data;
-		d.firstChild.onchange=c.onchange;
-		delete c.onchange;
-	}
+	if('symbol' in c) d.firstChild.innerText=c.symbol;
+	else if('data' in c) c.symbol='✓';
 	for(t in c) d[t]=c[t];
+	if('data' in c) loadItem(d,c.data);
 }
 function menuCommand(e){e=e.target;bg.postMessage('Command',null,e.cmd);}
 function menuScript(i) {
 	var s=bg.map[i];if(!s) return;
 	var n=(s.meta.name||'('+_('Null')+')').replace(/&/g,'&amp;').replace(/</g,'&lt;');
-	addItem(n,i[0],{data:s.enabled,onchange:function(e){
-		loadItem(this.parentNode,s.enabled=this.checked);bg.saveScripts();
+	addItem(n,i[0],{data:s.enabled,onclick:function(e){
+		loadItem(this,s.enabled=!s.enabled);bg.saveScripts();
 	}});
 }
 function load(e,data){
-	addItem(_('Manage scripts'),true,{onclick:function(){
+	addItem(_('Manage scripts'),true,{symbol:'>>',onclick:function(){
 		bg.opera.extension.tabs.create({url:'/options.html'}).focus();
 	}});
-	if(data) addItem(_('Search scripts for this site'),true,{onclick:function(){
+	if(data) addItem(_('Search scripts for this site'),true,{symbol:'>>',onclick:function(){
 		var q='site:userscripts.org+inurl:show+'+tab.url.replace(/^.*?:\/\/([^\/]*?)\.\w+\/.*$/,function(v,g){
 			return g.replace(/\.(com|..)$/,'').replace(/\./g,'+');
 		});
 		return bg.opera.extension.tabs.create({url:'http://www.baidu.com/s?wd='+q}).focus();
 		//return bg.opera.extension.tabs.create({url:'http://www.google.com.hk/search?q='+q}).focus();
 	}});
-	addItem(_('Scripts enabled'),true,{data:bg.isApplied,onchange:function(e){
-		loadItem(this.parentNode,bg.saveSetting('isApplied',bg.isApplied=this.checked));bg.updateIcon();
+	addItem(_('Scripts enabled'),true,{data:bg.isApplied,onclick:function(e){
+		loadItem(this,bg.saveSetting('isApplied',bg.isApplied=!bg.isApplied));bg.updateIcon();
 	}});
 	P.appendChild(document.createElement('hr'));
 	if(data&&data[0]&&data[0].length) {
-		for(var i=0;i<data[0].length;i++) addItem(data[0][i][0],true,{onclick:menuCommand,cmd:data[0][i][0]});
+		for(var i=0;i<data[0].length;i++) addItem(data[0][i][0],true,{symbol:'>>',onclick:menuCommand,cmd:data[0][i][0]});
 		P.appendChild(document.createElement('hr'));
 	}
-	if(data&&data[1]&&data[1].length) data[1].forEach(menuScript); else addItem('<em>'+_('Null')+'</em>',_('Null'),{className:'hint'});
+	if(data&&data[1]&&data[1].length) data[1].forEach(menuScript); else addItem('<em>'+_('Null')+'</em>',_('Null'),{className:'disabled'});
 	bg.button.popup.height=document.body.offsetHeight;
 }
 if(tab.port) bg.postMessage('GetPopup','GotPopup',null,load); else load();
