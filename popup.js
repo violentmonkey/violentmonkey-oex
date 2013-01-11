7f55baddf4c6f03cc37616a4ac9882638faff0e9
Gerald <gera2ld@163.com>
2013-01-11 23:38:03 +0800

    improved popup ui

--- popup.js
+++ popup.js
@@ -1,6 +1,8 @@
 function $(i){return document.getElementById(i);}
-var bg=opera.extension.bgProcess,P=$('popup'),_=bg.getI18nString,
-	tab=bg.opera.extension.tabs.getFocused();
+var bg=opera.extension.bgProcess,P=$('popup'),C=$('commands'),
+		pT=P.querySelector('.top'),pB=P.querySelector('.bot'),
+		cT=C.querySelector('.top'),cB=C.querySelector('.bot'),
+		_=bg.getI18nString,tab=bg.opera.extension.tabs.getFocused();
 function loadItem(d,c){
 	if(c) {
 		d.firstChild.innerText=d.symbol;
@@ -15,7 +17,7 @@ function addItem(h,t,c){
 	d.innerHTML='<span></span>'+h;
 	if(t) {if(typeof t!='string') t=h;d.title=t;}
 	d.className='ellipsis';
-	P.appendChild(d);
+	c.holder.appendChild(d);
 	if('symbol' in c) d.firstChild.innerText=c.symbol;
 	else if('data' in c) c.symbol='✓';
 	for(t in c) d[t]=c[t];
@@ -25,34 +27,44 @@ function menuCommand(e){e=e.target;tab.postMessage({topic:'Command',data:e.cmd})
 function menuScript(i) {
 	var s=bg.map[i];if(!s) return;
 	var n=s.meta.name?s.meta.name.replace(/&/g,'&amp;').replace(/</g,'&lt;'):'<em>'+_('Null name')+'</em>';
-	addItem(n,s.meta.name,{data:s.enabled,onclick:function(e){
+	addItem(n,s.meta.name,{holder:pB,data:s.enabled,onclick:function(e){
 		loadItem(this,s.enabled=!s.enabled);bg.saveScripts();
 	}});
 }
 function load(e,data){
-	addItem(_('Manage scripts'),true,{symbol:'➤',onclick:function(){
+	addItem(_('Manage scripts'),true,{holder:pT,symbol:'➤',onclick:function(){
 		var t=bg.opera.extension.tabs.create({url:'/options.html'});
 		if(t.focus) t.focus();	// Opera 12+ Only
 	}});
-	if(data) addItem(_('Find scripts for this site'),true,{symbol:'➤',onclick:function(){
+	if(data) addItem(_('Find scripts for this site'),true,{holder:pT,symbol:'➤',onclick:function(){
 		var q='site:userscripts.org+inurl:show+'+tab.url.replace(/^.*?:\/\/([^\/]*?)\.\w+\/.*$/,function(v,g){
 			return g.replace(/\.(com|..)$/,'').replace(/\./g,'+');
 		}),url=bg.format(bg.search,q);
 		var t=bg.opera.extension.tabs.create({url:url});
 		if(t.focus) t.focus();	// Opera 12+ Only
 	}});
-	addItem(_('Scripts enabled'),true,{data:bg.isApplied,onclick:function(e){
+	if(data&&data[0]&&data[0].length) {
+		addItem(_('Back'),true,{holder:cT,symbol:'◄',onclick:function(){
+			C.classList.add('hide');P.classList.remove('hide');
+			bg.button.popup.height=P.offsetHeight;
+		}});
+		cT.appendChild(document.createElement('hr'));
+		data[0].forEach(function(i){addItem(i[0],true,{holder:cB,symbol:'➤',onclick:menuCommand,cmd:i[0]});});
+		addItem(_('Script commands...'),true,{holder:pT,symbol:'➤',onclick:function(){
+			P.classList.add('hide');C.classList.remove('hide');
+			bg.button.popup.height=C.offsetHeight;
+			setTimeout(function(){cB.style.pixelHeight=innerHeight-cB.offsetTop;},0);
+		}});
+	}
+	addItem(_('Scripts enabled'),true,{holder:pT,data:bg.isApplied,onclick:function(e){
 		bg.saveSetting('isApplied',bg.isApplied=!bg.isApplied);bg.updateIcon();loadItem(this,bg.isApplied);
 	}});
-	if(data&&data[0]&&data[0].length) {
-		P.appendChild(document.createElement('hr'));
-		for(var i=0;i<data[0].length;i++) addItem(data[0][i][0],true,{symbol:'➤',onclick:menuCommand,cmd:data[0][i][0]});
-	}
 	if(data&&data[1]&&data[1].length) {
-		P.appendChild(document.createElement('hr'));
+		pT.appendChild(document.createElement('hr'));
 		data[1].forEach(menuScript);
 	}
-	bg.button.popup.height=document.body.offsetHeight;
+	bg.button.popup.height=P.offsetHeight;
+	setTimeout(function(){pB.style.pixelHeight=innerHeight-pB.offsetTop;},0);
 }
 bg.messages['GotPopup']=load;
 try{tab.postMessage({topic:'GetPopup'});}catch(e){load();}
