b7e40a56daf9c3817d217dd151a58f6537dbdd5e
Gerald <gera2ld@163.com>
2013-01-26 02:48:32 +0800

    improved wrapper

--- includes/injected.js
+++ includes/injected.js
@@ -59,11 +59,8 @@ opera.extension.addEventListener('message', function(e) {
 	else if(message.topic=='HttpRequested') {
 		c=requests[message.data.id];
 		if(c) c.callback(message.data);
-	} else if(message.topic=='LoadedCache') {
-		cache=message.data;document.onreadystatechange=runScript;
-		window.addEventListener('DOMNodeInserted',runScript,false);
-		runScript();
-	} else if(message.topic=='GetPopup')
+	} else if(message.topic=='LoadedCache') initRunScript(message.data);
+	else if(message.topic=='GetPopup')
 		opera.extension.postMessage({topic:'GotPopup',data:[menu,scr]});
 	else if(message.topic=='Command') {
 		c=command[message.data];if(c) c();
@@ -133,15 +130,16 @@ if(/\.user\.js$/.test(window.location.href)) (function(){
 // For injected scripts
 var start=[],body=[],end=[],cache={},scr=[],menu=[],command={};
 function run_code(c){
-	this.wrapper=new wrapper(c);
-	var require=c.meta.require||[],i,r;
+	var w=new wrapper(c),require=c.meta.require||[],i,r,f,code=[];
+	code.push('try{');
 	for(i=0;i<require.length;i++) try{
 		r=cache[require[i]];if(!r) continue;
-		this.code=utf8decode(r);
-		with(this.wrapper) try{eval(this.code);}catch(e){opera.postError(e+'\n'+e.stacktrace);}
+		code.push(utf8decode(r));
 	}catch(e){opera.postError(e+'\n'+e.stacktrace);}
-	this.code=c.code;
-	with(this.wrapper) try{eval('(function(){'+this.code+'})();');}catch(e){opera.postError(e+'\n'+e.stacktrace);}
+	code.push(c.code);
+	code.push('}catch(e){opera.postError(e+"\\n"+e.stacktrace);}');
+	this.code=code.join('\n');
+	try{with(w) eval(this.code);}catch(e){opera.postError(e+'\n'+e.stacktrace);}
 }
 function runScript(e){
 	function onreadystatechange(){
@@ -158,8 +156,13 @@ function runScript(e){
 	if(!e||e.type=='readystatechange') onreadystatechange();
 	if(!e||e.type=='DOMNodeInserted') onDOMNodeInserted();
 }
+function initRunScript(data){
+	if(data) cache=data;document.onreadystatechange=runScript;
+	window.addEventListener('DOMNodeInserted',runScript,false);
+	runScript();
+}
 function loadScript(data){
-	var l;
+	var l,c=null;
 	data.data.forEach(function(i){
 		scr.push(i.id);
 		if(data.isApplied&&i.enabled) {
@@ -169,38 +172,23 @@ function loadScript(data){
 				default: l=end;
 			}
 			l.push(i);
-			if(i.meta.require) i.meta.require.forEach(function(i){cache[i]=null;});
-			for(l in i.meta.resources) cache[i.meta.resources[l]]=null;
+			if(i.meta.require) i.meta.require.forEach(function(i){cache[c=i]=null;});
+			for(l in i.meta.resources) cache[c=i.meta.resources[l]]=null;
 		}
 	});
-	opera.extension.postMessage({topic:'LoadCache',data:cache});
-}
-function wrapFunction(o,i,c){
-	var f=function(){var r=o[i].apply(o,arguments);if(c) r=c(r);return r;};
-	/*var p=function(){};
-	p.prototype=o[i].prototype;
-	f.prototype=new p();*/
-	return f;
-}
-function each(obj, fn) {
-	if(typeof Object.getOwnPropertyNames === 'function') {
-		Object.getOwnPropertyNames(obj).forEach(fn);
-	} else {
-		for(var i in obj) {
-			fn(i);
-		}
-	}
+	if(c) opera.extension.postMessage({topic:'LoadCache',data:cache});
+	else initRunScript();
 }
 function wrapper(c){
-	var t=this;
-	t.unsafeWindow=window;
-	var ns=c.meta.namespace||'',n=c.meta.name||'',ckey='val:'+escape(ns)+':'+escape(n)+':';
-	if(!ns&&!n) ckey+=n.id;ckey+=':';
+	var t=c.meta.namespace||'',n=c.meta.name||'',ckey='val:'+escape(t)+':'+escape(n)+':';
+	if(!t&&!n) ckey+=n.id;ckey+=':';t=this;
+	function addProperty(name,prop){t[name]=prop;}
 	var resources=c.meta.resources||{};
+	addProperty('unsafeWindow',window);
 	// GM functions
 	// Reference: http://wiki.greasespot.net/Greasemonkey_Manual:API
-	t.GM_deleteValue=function(key){widget.preferences.removeItem(ckey+key);};
-	t.GM_getValue=function(key,def){
+	addProperty('GM_deleteValue',function(key){widget.preferences.removeItem(ckey+key);});
+	addProperty('GM_getValue',function(key,def){
 		var v=widget.preferences.getItem(ckey+key);
 		if(v==null) return def;
 		def=v.substr(1);
@@ -209,61 +197,70 @@ function wrapper(c){
 			case 'b': return !!JSON.parse(def);
 			default: return def;
 		}
-	};
-	t.GM_listValues=function(){
+	});
+	addProperty('GM_listValues',function(){
 		var v=[],i,l=ckey.length,k;
 		for(i=0;i<widget.preferences.length;i++) {
 			k=widget.preferences.key(i);
 			if(k.substr(0,l)==ckey) v.push(k.substr(l));
 		}
 		return v;
-	};
-	t.GM_setValue=function(key,val){
+	});
+	addProperty('GM_setValue',function(key,val){
 		switch(typeof val){
 			case 'number':val='n'+val;break;
 			case 'boolean':val='b'+val;break;
 			default:val='s'+val;
 		}
 		widget.preferences.setItem(ckey+key,val);
-	};
+	});
 	function getCache(name){for(var i in resources) if(name==i) return cache[resources[i]];}
-	t.GM_getResourceText=function(name){
+	addProperty('GM_getResourceText',function(name){
 		var b=getCache(name);
 		if(b) b=utf8decode(b);
 		return b;
-	};
-	t.GM_getResourceURL=function(name){
+	});
+	addProperty('GM_getResourceURL',function(name){
 		var b=getCache(name);
 		if(b) b='data:;base64,'+base64encode(b);
 		return b;
-	};
-	t.GM_addStyle=function(css){
+	});
+	addProperty('GM_addStyle',function(css){
 		if(!document.head) return;
 		var v=document.createElement('style');
 		v.innerHTML=css;
 		document.head.appendChild(v);
 		return v;
-	};
-	t.GM_log=console.log;
-	t.GM_openInTab=function(url){window.open(url);};
-	t.GM_registerMenuCommand=function(cap,func,acc){menu.push([cap,acc]);command[cap]=func;};
-	t.GM_xmlhttpRequest=function(details){
+	});
+	addProperty('GM_log',console.log);
+	addProperty('GM_openInTab',function(url){window.open(url);});
+	addProperty('GM_registerMenuCommand',function(cap,func,acc){menu.push([cap,acc]);command[cap]=func;});
+	addProperty('GM_xmlhttpRequest',function(details){
 		// synchronous mode not supported
 		var r=new Request(details);
 		return r.req;
-	};
-	t.VM_info={version:widget.version};
+	});
+	addProperty('VM_info',{version:widget.version});
 	// functions and properties
+	function wrapFunction(o,i,c){
+		var f=function(){var r=o[i].apply(o,arguments);if(c) r=c(r);return r;};
+		return f;
+	}
 	function wrapWindow(w){return w==window?t:w;}
-	each(window, function(i) { try {
-		if(typeof window[i]=='function') t[i]=wrapFunction(window,i,wrapWindow);
-		else if(window[i] instanceof Object) t[i]=wrapWindow(window[i]);
-		else {
-			t.__defineGetter__(i, function() {return wrapWindow(window[i]);});
-			t.__defineSetter__(i, function(v) { window[i] = v; });
-		}
-	} catch(e) {}});	// avoid reading protected data*/
+	function wrapItem(i,nowrap){
+	       	try{	// avoid reading protected data*/
+			if(typeof window[i]=='function') {
+				if(nowrap) t[i]=window[i];
+				else t[i]=wrapFunction(window,i,wrapWindow);
+			}
+			else {
+				t.__defineGetter__(i,function(){return wrapWindow(window[i]);});
+				t.__defineSetter__(i,function(v){window[i]=v;});
+			}
+		}catch(e){}
+	}
+	Object.getOwnPropertyNames(window).forEach(wrapItem);
+	for(n in window.Window.prototype) wrapItem(n);
 }
-wrapper.prototype=window;
 opera.extension.postMessage({topic:'FindScript',data:window.location.href});
 })();
