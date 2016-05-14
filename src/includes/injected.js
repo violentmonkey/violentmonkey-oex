2c38c53f47a27d124e8468dfdb889f647bf87e75
Gerald <gera2ld@163.com>
2016-05-14 23:07:32 +0800

    fix: build function with `window` as context
    fix #45

--- src/includes/injected.js
+++ src/includes/injected.js
@@ -533,7 +533,11 @@ if (document.contentType !== 'text/html' && /\.user\.js$/i.test(window.location.
         var name = script.custom.name || script.meta.name || script.id;
         // normal injection
         try {
-          var func = new Function('g', code);
+          // In content page of Opera Presto, the top level Window-like object
+          // `this` is different from `window`. In order to build the function
+          // with prototypes from `window`, we MUST use `window.Function`
+          // instead of `Function`.
+          var func = new window.Function('g', code);
         } catch(e) {
           console.error('Syntax error in script: ' + name + '\n' + e.message);
           return;
